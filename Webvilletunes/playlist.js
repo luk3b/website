7db88743d940e4270a1f623945a4dec434461960
luk3b <143647306+luk3b@users.noreply.github.com>
2024-02-29 14:31:57 -0500

    Delete Webvilletunes directory

--- Webvilletunes/playlist.js
+++
@@ -1,8 +0,0 @@
-function setUpButtonHandler() {
-    let button = document.getElementById("addButton");
-    button.onclick = handleButtonClick;
-}
-
-function handleButtonClick() {
-    alert("Button was clicked!");
-}
--- Webvilletunes/setlinks.js
+++
@@ -1,8 +0,0 @@
-function setValidationLinks() {
-    let loc = window.location.href;
-    let HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
-    let CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
-                          loc + '?profile=css3';
-    document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
-    document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
-  }
